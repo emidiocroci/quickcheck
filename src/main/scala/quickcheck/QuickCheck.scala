e943cdfcb3197ba82ba27a5a31b5fe8ae110b3c2
Emidio Croci <emidio.croci@gmail.com>
2013-11-10 09:12:57 +0100

    FIxed gitignore Implemented generator

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -1,4 +1,7 @@
 package quickcheck
+// > <console>:1: error: illegal start of definition
+// >        package quickcheck
+// >        ^
 
 import common._
 
@@ -14,11 +17,28 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(h) == a
   }
 
-  lazy val genHeap: Gen[H] = for{
-    isEmpty <- arbitrary[Boolean] 
-    item <- if (isEmpty) empty else insert(scala.util.Random.nextInt(), genHeap)
-  } yield item
+  property("min2") = forAll { (a: Int, b: Int) =>
+    val h = insert(a, empty)
+    insert(b, h)
+    /*println(findMin(h))
+    println(scala.math.min(a, b))*/
+    findMin(h) == scala.math.min(a, b)
+  }
+
+  property("delMin") = forAll { a: Int =>
+  	val h = insert(a, empty)
+  	deleteMin(h) == empty
+  }
+  
+  property("minMeld") = forAll { (a: H, b: H) =>
+ 	val melded = meld(a, b)
+ 	findMin(melded) == findMin(a) || findMin(melded) == findMin(b)
+  }  
+
+  lazy val genHeap: Gen[H] = for {    
+    x <- scala.util.Random.nextInt()
+    item <- oneOf(empty, genHeap)
+  } yield meld(empty,insert(x, empty))
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
-
 }
