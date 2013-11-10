2b13c1761adaf0c5d214526a4067dd42ee101150
Emidio Croci <emidio.croci@gmail.com>
2013-11-10 19:10:03 +0100

    completed tests

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -19,21 +19,32 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   property("min2") = forAll { (a: Int, b: Int) =>
     val h = insert(a, empty)
-    insert(b, h)
-    /*println(findMin(h))
-    println(scala.math.min(a, b))*/
+    insert(b, h) 
     findMin(h) == scala.math.min(a, b)
   }
 
   property("delMin") = forAll { a: Int =>
   	val h = insert(a, empty)
-  	deleteMin(h) == empty
+    isEmpty(deleteMin(h))
   }
   
   property("minMeld") = forAll { (a: H, b: H) =>
- 	val melded = meld(a, b)
- 	findMin(melded) == findMin(a) || findMin(melded) == findMin(b)
-  }  
+ 	  val melded = meld(a, b)
+    print(findMin(melded) + " " + findMin(a) + " " + findMin(b))
+ 	  findMin(melded) == findMin(a) || findMin(melded) == findMin(b)
+  }    
+
+  property("sortedHeap") = forAll { a: H =>
+    def isSorted(min: Int, h: H): Boolean = 
+      if (isEmpty(h))
+        true
+      else if( min > findMin(h) )
+        false
+      else
+        isSorted(findMin(h), deleteMin(h))
+
+    isSorted(findMin(a), deleteMin(a))
+  }
 
   lazy val genHeap: Gen[H] = for {    
     x <- scala.util.Random.nextInt()
